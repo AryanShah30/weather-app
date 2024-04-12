e0bd359e1905016bd08e29e47a53db4a285c99fc
Aryan Shah <115439830+AryanShah30@users.noreply.github.com>
2024-04-12 22:10:34 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      <a href="https://www.linkedin.com/in/aryanashah/"> Aryan Shah (22BCE2741) </a>
+    </p>
+  </div>
+  );
+}
